43d342a97579135dd2c4034d6179ec26753b2632
Iustin Nita <iustin.nitza@gmail.com>
2020-09-10 17:45:04 +0200

    add hud for score

--- Main.gd
+++ Main.gd
@@ -4,11 +4,15 @@ var Circle = preload("res://objects/Circle.tscn")
 var Jumper = preload("res://objects/Jumper.tscn")
 
 var player
+var score = 0
 
 func _ready():
 	randomize()
+	$HUD.hide()
 
 func new_game():
+	score = 0
+	$HUD.update_score(score)
 	$Camera2D.position = $StartingPosition.position
 	player = Jumper.instance()
 	player.position = $StartingPosition.position
@@ -16,6 +20,8 @@ func new_game():
 	player.connect("captured", self, "_on_Jumper_captured")
 	player.connect("died", self, "_on_Jumper_died")
 	spawn_circle($StartingPosition.position)
+	$HUD.show()
+	$HUD.show_message("Go!")
 	
 func spawn_circle(_position=null):
 	var c = Circle.instance()
@@ -33,7 +39,10 @@ func _on_Jumper_captured(object):
 	# can't call directly spawn_circle() because we are 
 	# changing the physics state during physics processing
 	call_deferred("spawn_circle")
+	score += 1
+	$HUD.update_score(score)
 
 func _on_Jumper_died():
 	get_tree().call_group("circles", "implode") # lets us call a function on each member of the group
 	$Screens.game_over()
+	$HUD.hide()
